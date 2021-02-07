97262df3860acbdbe8107aebd685bf0dadc1fc11
Scharif47 <61374955+Scharif47@users.noreply.github.com>
2021-02-07 19:00:27 +0100

    init commit

---
+++ app/js/script.js
@@ -0,0 +1,27 @@
+const btnHamburger = document.getElementById("btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", function () {
+  console.log("open hamburger");
+
+  if (header.classList.contains("open")) {
+    // Close hamburger menu
+    header.classList.remove("open");
+    body.classList.remove("noScroll");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
+  } else {
+    // Open hamburger menu
+    header.classList.add("open");
+    body.classList.add("noScroll");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-out");
+      element.classList.add("fade-in");
+    });
+  }
+});
